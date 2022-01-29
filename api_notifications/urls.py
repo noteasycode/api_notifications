d0e746a2f2ce5476541f83f3b7a77e9ab42c4bf0
Denis <denis.kr.pr@gmail.com>
2022-01-29 23:43:11 +0200

    do some progress

--- api_notifications/urls.py
+++ api_notifications/urls.py
@@ -5,10 +5,10 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('notifications.urls')),
-    path('admin/', admin.site.urls),
     path('auth/', include('users.urls', namespace='users')),
     path('auth/', include('django.contrib.auth.urls')),
+    path('', include('notifications.urls')),
+    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
