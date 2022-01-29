8504d716cc16ce56b16b8a1214a07c36e7b8ad74
Denis <denis.kr.pr@gmail.com>
2022-01-30 00:25:51 +0200

    do some progress

--- api_notifications/settings.py
+++ api_notifications/settings.py
@@ -135,4 +135,4 @@ LOGOUT_REDIRECT_URL = 'notifications:index'
 
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, '../sent_emails')
