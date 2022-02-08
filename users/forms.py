a2f853e18026b64f46438fe2254c3548115657cd
Denis <denis.kr.pr@gmail.com>
2022-02-08 15:45:02 +0200

    add user change form

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,32 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import forms import
 from django.contrib.auth import get_user_model
 
 
 User = get_user_model()
 
 
-class CreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
+class CreationForm(forms.UserCreationForm):
+    email = forms.Email
+    class Meta(forms.UserCreationForm.Meta):
         model = User
-        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'avatar')
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2',
+            'avatar'
+        )
+
+
+class UserUpdateForm(forms.UserChangeForm):
+    class Meta(forms.UserChangeForm.Meta):
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'avatar'
+        )
