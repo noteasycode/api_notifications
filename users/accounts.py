0c0841a941678b0ec1951369dfdde3fa53b7263c
Denis <denis.kr.pr@gmail.com>
2022-02-11 17:34:15 +0200

    add new admin area for managing user accounts

--- users/accounts.py
+++ users/accounts.py
@@ -11,7 +11,35 @@ class AccountAdminArea(admin.AdminSite):
     site_header = 'Accounts'
 
 
+class NotificationAdminPermission(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_view_permission(self, request, obj=None):
+        return True
+
+
+class CustomUserAdminPermission(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+    def has_view_permission(self, request, obj=None):
+        return True
+
+
 accounts_site = AccountAdminArea(name='AccountAdmin')
 
-accounts_site.register(CustomUser)
-accounts_site.register(Notification)
+accounts_site.register(CustomUser, CustomUserAdminPermission)
+accounts_site.register(Notification, NotificationAdminPermission)
