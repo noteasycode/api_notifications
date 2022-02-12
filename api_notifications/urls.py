b9976ad5cb11e53b5e1e104ff4dd9dd6bc74b126
Denis <denis.kr.pr@gmail.com>
2022-02-12 16:51:50 +0200

    update admin page for managing accounts

--- api_notifications/urls.py
+++ api_notifications/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.i18n import JavaScriptCatalog
 from django.urls import path, include
 
 from users.accounts import accounts_site
@@ -11,7 +10,6 @@ urlpatterns = [
     path('auth/', include('users.urls', namespace='users')),
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('notifications.urls')),
-    path('jsi18n', JavaScriptCatalog.as_view(), name='js-catalog'),
     path('accounts/', accounts_site.urls),
     path('admin/', admin.site.urls),
 ]
--- users/accounts.py
+++ users/accounts.py
@@ -7,11 +7,24 @@ from notifications.models import Notification
 CustomUser = get_user_model()
 
 
+@admin.action(description='Block selected users')
+def block_user(modeladmin, request, queryset):
+    queryset.update(is_active='False')
+
+
+@admin.action(description='Unblock selected users')
+def unblock_user(modeladmin, request, queryset):
+    queryset.update(is_active='True')
+
+
 class AccountAdminArea(admin.AdminSite):
     site_header = 'Accounts'
 
 
-class NotificationAdminPermission(admin.ModelAdmin):
+class NotificationAdmin(admin.ModelAdmin):
+    model = Notification
+    list_display = ('name', 'text', 'notify_date', 'receiver')
+
     def has_add_permission(self, request):
         return False
 
@@ -25,7 +38,12 @@ class NotificationAdminPermission(admin.ModelAdmin):
         return True
 
 
-class CustomUserAdminPermission(admin.ModelAdmin):
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'avatar')
+    list_filter = ('is_active',)
+    actions = [block_user, unblock_user]
+
     def has_add_permission(self, request):
         return False
 
@@ -40,6 +58,5 @@ class CustomUserAdminPermission(admin.ModelAdmin):
 
 
 accounts_site = AccountAdminArea(name='AccountAdmin')
-
-accounts_site.register(CustomUser, CustomUserAdminPermission)
-accounts_site.register(Notification, NotificationAdminPermission)
+accounts_site.register(CustomUser, CustomUserAdmin)
+accounts_site.register(Notification, NotificationAdmin)
